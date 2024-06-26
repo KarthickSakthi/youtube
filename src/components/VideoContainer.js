1b5f7b5ed3f4b31965a9099e885e1980e0df3931
KarthickSakthi <karthicksakthi124@gmail.com>
2024-06-27 00:00:09 +0530

    Video omponent added

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEO_API } from "../constant";
+import VideoCard from "./VideoCard";
 
 export function VideoContainer(){
-    return(<h1>VideoContainer component </h1>)
+  const [videos, setVideos] = useState([]);
+
+    useEffect(function getVideos(){
+        fetchVideos();
+    },[]);
+
+     const  fetchVideos=async()=>{
+        const videosData = await fetch(YOUTUBE_VIDEO_API);
+        const response = await videosData.json();;
+        console.log("VideoContainer", response)
+        setVideos(response.items);
+    }
+
+    return(<div className="flex flex-wrap">
+    {videos.map(video=> <VideoCard key={video.id} info={video}/>)}
+    </div>)
 }
