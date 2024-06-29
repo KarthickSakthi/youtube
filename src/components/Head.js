a8f17de20c3ca095ea363c2ded916ba2d83c8dbd
KarthickSakthi <karthicksakthi124@gmail.com>
2024-06-29 22:20:32 +0530

    Youtube Search widget changes

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./../images/logo.png"
 import Menu from "./../images/menu.png"
 import User from "./../images/user.png"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../store/appSlice";
+import { YOUTUBE_SEARCH_API } from "../constant";
+import { cacheResults } from "../store/searchSlice";
 
 export function Head(){
+    const [searchQuery , setSearchQuery] = useState();
+    const [suggestions, setSuggestions] = useState([]);
+    const searchCache = useSelector(store =>store.search);
     const dispatch = useDispatch();
+
+    useEffect(function debounceSearcQuery(){
+        let timer = searchQuery && setTimeout(()=>{
+            if(searchCache[searchQuery]){
+                setSuggestions(searchCache[searchQuery])
+            }
+            else{
+            generateSuggestions();}},500)
+
+        return function cleanUp(){
+            clearTimeout(timer);
+        }
+    },[searchQuery]);
+
+
+    async function generateSuggestions(){
+     const suggestion = await fetch(YOUTUBE_SEARCH_API+searchQuery);
+     const result = await suggestion.json();
+      console.log({result});
+      setSuggestions(result[1]);
+      dispatch(cacheResults({[searchQuery]: result[1]}));
+    }
     
     function toggleMenuHandler(){
         dispatch(toggleMenu())
@@ -18,8 +45,18 @@ export function Head(){
              <img className="h-8" alt ="logo" src={logo}/>
             </div>
             <div className="col-span-10 px-10 ">
-               <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text"/> 
+                <div>
+               <input className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" onChange={(event)=> setSearchQuery(event.target.value)}
+               onBlur={()=>setSuggestions([])}
+               /> 
                <button className="border border-gray-400 py-2 px-5 rounded-r-full bg-gray-100"> 🔍 </button>
+               </div>
+         
+            <div className="fixed bg-white py-2 px-5 w-[37rem] shadow-lg rounded-lg border-gray-100">
+           <ul>
+            {suggestions.map((suggestion)=> <li key={suggestion} className="py-2 shadow-sm hover:bg-gray-100">🔍 {suggestion}</li>)}
+           </ul>
+            </div>
             </div>
             <div className="col-span-1">
             <img className="h-8" alt="user" src={User}/>  
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -5,10 +5,10 @@ function VideoCard({info}){
     const {snippet, statistics} = info;
     const {channelTitle, title, thumbnails} = snippet;
     return(
-        <div className="p-2 m-2 w-72 shadow-lg">
+        <div className="p-2 m-2 w-72 max-h-80 shadow-lg">
           <img className="rounded-lg" alt="thumbnail" src={thumbnails.medium.url}/>
           <ul>
-            <li className="font-bold py-2">{title}</li>
+            <li className="font-bold py-2 w-64 h-16 leading-7  text-ellipsis line-clamp-2 overflow-hidden text-pretty">{title}</li>
             <li>{channelTitle}</li>
             <li>{statistics.viewCount} views</li>
           </ul>
