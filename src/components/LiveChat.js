96064330bbc57e5a228c6db21088f441a0b96a8d
KarthickSakthi <karthicksakthi124@gmail.com>
2024-07-04 09:33:06 +0530

    Live chat chenges

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../store/chatSlice";
+import { generateRandomName, generateRandomString } from "../utils";
 
 
 function LiveChat(){
+    const dispatch = useDispatch();
+    const messages = useSelector(store=> store.chat.messages);
+
+    useEffect(function getMessages(){
+        const timer = setInterval(()=>dispatch(addMessage({
+         name: generateRandomName(),
+         message: generateRandomString(25) + " 😁"
+        })),1000);
+
+        return ()=>{
+            clearInterval(timer)
+        }
+    },[])
     return(
-        <div roun className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg">
-         <ChatMessage 
-         name={"Karthick Sakthi"}
-         message={"Hi all"}
-         />
+        <div  className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+            {
+                messages.map((chat)=>(
+                    <ChatMessage 
+                    name={chat.name}
+                    message={chat.message}
+                    />
+                ))
+            }
+
         </div>
     )
 }
